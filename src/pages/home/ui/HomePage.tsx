103a27445b5cb25047965dc95946ebe1a2ef155e
Gleb Kornilov <esplete@gmail.com>
2025-08-15 20:00:18 +0300

    feat: Add Loading Component with HOC for PostList

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,12 +1,33 @@
 import styles from './HomePage.module.css';
-import PostList from '../../../widgets/PostList';
+import {useEffect, useState} from 'react';
+import {type Post, MOCK_POSTS} from '../../../shared/constants/constants.ts';
+import PostListWithLoading from '../../../widgets/PostList';
 
 
 const HomePage = () => {
+
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setPosts(MOCK_POSTS);
+      setIsLoading(false);
+
+      return () => clearTimeout(timer);
+    }, 2000);
+  }, []);
+
+
+
+
   return (
-    <div className={styles.homePage}>
+    <div className={styles.container}>
       <h1 className={styles.title}>Лента постов</h1>
-      <PostList />
+      <div className={styles.contentWrapper}>
+        <PostListWithLoading isLoading={isLoading} posts={posts}/>
+      </div>
     </div>
   );
 };
